2ed89878dc996a32da76d042c360a2f16c25184b
alans <alanshenjc@gmail.com>
2024-09-07 00:35:43 +0300

    added footers

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ import pandas as pd
 
 pdf = FPDF(orientation='P', unit='mm', format='A4')
 # p stands for portrait, l stands for land scape, unit is the dimension so mm in this case
+pdf.set_auto_page_break(auto=False, margin=0)
+# to have page not be broken automatically, which set to false
+# as the footer was not aligned properly
 
 df = pd.read_csv('topics (1).csv')
 
@@ -14,12 +17,28 @@ for index, row in df.iterrows():
 
     pdf.cell(w=0, h=12, txt=row['Topic'], align='L', ln=1)
     pdf.line(x1=10, y1=21, x2=200, y2=21)  # this is a add a line below the topic name
+    # add footer for page with title
+    pdf.ln(265)  # this is the size of the break lines, 278 because A4 is around 297 height
+    # which should fit nicely with 265, reduce accordingly if don't see the footer
+    pdf.set_font(family='Times', style='B', size=8)
+    pdf.set_text_color(100, 100, 100)
+    pdf.cell(w=0, h=10, txt=row['Topic'], align='R', ln=1)
 
     # if we want another font for another cell we need to have another set_font above the cell.
-    for page in range(row['Pages']):
+    for page in range(row['Pages'] - 1):
         pdf.add_page()
         # this is to add pages according to the pages in the csv
         # in range is used for the range numbers in the pages colum
+
+        # add footers for other page with no titles
+        pdf.ln(277)  # this is 265 +h=12 = 277, as this counts from top of the page
+
+        # this is the size of the break lines, 278 because A4 is around 297 height
+        # which should fit nicely with 265, reduce accordingly if you don't see the footer
+        pdf.set_font(family='Times', style='B', size=8)
+        pdf.set_text_color(100, 100, 100)
+        pdf.cell(w=0, h=10, txt=row['Topic'], align='R', ln=1)
+        # align to the right.
 
 pdf.output('output.pdf')
 
